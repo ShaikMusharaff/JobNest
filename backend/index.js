eaa15d239e6e77ff26b61393352d02c877d9ed95
Musharaff <smusharaf666@gmail.com>
2025-10-08 19:15:40 +0530

    added mongo schemas

--- backend/index.js
+++ backend/index.js
@@ -1,1 +1,27 @@
-import express from "express"
+import express from "express";
+import cookieParser from "cookie-parser";
+import cors from "cors";
+import dotenv from "dotenv";
+import connectDB from "./utils/db.js";
+
+dotenv.config(); // Load .env variables
+
+const app = express();
+
+// Middleware
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(cookieParser());
+
+const corsOptions = {
+    origin: ["http://localhost:5121"],
+    credentials: true,
+};
+app.use(cors(corsOptions));
+
+const PORT = process.env.PORT || 5001;
+
+app.listen(PORT, () => {
+    connectDB();
+    console.log(`Server is running on port ${PORT}`);
+});
